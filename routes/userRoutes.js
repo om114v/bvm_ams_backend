32bb7a614d1ecf48891aba2b91de71a6e7f89cec
Om Vanara <om114vanara@gmail.com>
2024-08-29 15:39:03 +0530

    update and delete api for user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,4 +6,7 @@ export const userRouter = express.Router();
 userRouter.post('/userRegistration', userController.userRegistration);
 userRouter.get('/getUsers', userController.getUsers);
 userRouter.get('/getUser/:userID', userController.getUser);
+userRouter.put('/updateUser/:userID', userController.updateUser);
+userRouter.delete('/deleteUser/:userID', userController.deleteUser);
+
 
