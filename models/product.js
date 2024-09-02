5f18c15607b6cd42b4a5833fd67ec879c10be314
Om Vanara <om114vanara@gmail.com>
2024-09-02 15:23:11 +0530

    added location in procuct

--- models/product.js
+++ models/product.js
@@ -3,9 +3,11 @@ import mongoose from 'mongoose';
 const productSchema = new mongoose.Schema(
     {
         productId: { type: Number, required: true, unique: true },
+        // orderId: [{ type: mongoose.Schema.Types.ObjectId, required: true, ref: 'Order' }],
         productName: { type: String, required: true },
         productCategory: { type: String, required: true },
-        specification: { type: String, required: true }
+        specification: { type: String, required: true },
+        location: { type: String, required: true },
     },
     { timestamps: true }
 );
