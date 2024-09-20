24756d8ef87b0c1db3e35da46c2a98900bae0943
Om Vanara <om114vanara@gmail.com>
2024-09-20 23:55:01 +0530

    partial implimented product model

--- models/product.js
+++ models/product.js
@@ -2,14 +2,140 @@ import mongoose from 'mongoose';
 
 const productSchema = new mongoose.Schema(
     {
-        productId: { type: Number, required: true, unique: true },
-        // orderId: [{ type: mongoose.Schema.Types.ObjectId, required: true, ref: 'Order' }],
+        productId: { type: String, required: true },
         productName: { type: String, required: true },
-        productCategory: { type: String, required: true },
-        specification: { type: String, required: true },
+        type: {
+            type: String,
+            required: true,
+            enum: ["AllInOne", "CPU", "Keyboard", "Mouse", "Monitor"],
+        },
+        model: { type: String, required: true },
+        purchaseDate: { type: Date, required: true },
+        specification: { type: String },
         location: { type: String, required: true },
     },
-    { timestamps: true }
+    {
+        timestamps: true,
+        discriminatorKey: "type",
+    }
 );
 
-export const productModel = mongoose.model('Product', productSchema);
+const allInOneSchema = new mongoose.Schema(
+    {
+        processor: { type: String, required: true },
+        generations: { type: Number, required: true },
+        cores: { type: Number, required: true },
+        l3Cache: { type: Number, required: true },
+        threads: { type: Number, required: true },
+        baseClockSpeed: { type: Number, required: true },
+        turboClockSpeed: { type: Number, required: true },
+        power: { type: Number, required: true },
+        ram: [{
+            size: { type: Number, required: true },
+            type: { type: String, required: true }
+        }],
+        totalRam: { type: Number, required: true },
+        hdd: [{
+            size: { type: Number, required: true },
+            type: { type: String, required: true }
+        }],
+        totalHdd: { type: Number, required: true },
+        ssd: [{
+            size: { type: Number, required: true },
+            type: { type: String, required: true }
+        }],
+        totalSsd: { type: Number, required: true },
+        graphicsCard: [{
+            size: { type: Number, required: true },
+            type: { type: String, required: true }
+        }],
+        screnType: { type: String, required: true },
+    }
+);
+
+const cpuSchema = new mongoose.Schema(
+    {
+        processor: { type: String, required: true },
+        generations: { type: Number, required: true },
+        cores: { type: Number, required: true },
+        l3Cache: { type: Number, required: true },
+        threads: { type: Number, required: true },
+        baseClockSpeed: { type: Number, required: true },
+        turboClockSpeed: { type: Number, required: true },
+        power: { type: Number, required: true },
+        ram: [{
+            size: { type: Number, required: true },
+            type: { type: String, required: true }
+        }],
+        totalRam: { type: Number, required: true },
+        hdd: [{
+            size: { type: Number, required: true },
+            type: { type: String, required: true }
+        }],
+        totalHdd: { type: Number, required: true },
+        ssd: [{
+            size: { type: Number, required: true },
+            type: { type: String, required: true }
+        }],
+        totalSsd: { type: Number, required: true },
+        motherBoard: { type: String, required: true },
+        graphicsCard: [{
+            size: { type: Number, required: true },
+            type: { type: String, required: true }
+        }],
+    }
+);
+
+const monitorSchema = new mongoose.Schema(
+    {
+        screenType: { type: String, required: true },
+    }
+);
+
+const mouseSchema = new mongoose.Schema(
+    {
+        mouseType: { type: String, required: true },
+    }
+);
+
+const keyboardSchema = new mongoose.Schema(
+    {
+        keyboardType: { type: String, required: true },
+    }
+);
+
+const productModel = mongoose.model('Product', productSchema);
+
+const allInOneModel = productModel.discriminator(
+    "AllInOne",
+    allInOneSchema
+);
+
+const cpuModel = productModel.discriminator(
+    "CPU",
+    cpuSchema
+);
+
+const monitorModel = productModel.discriminator(
+    "Monitor",
+    monitorSchema
+);
+
+const mouseModel = productModel.discriminator(
+    "Mouse",
+    mouseSchema
+);
+
+const keyboardModel = productModel.discriminator(
+    "Keyboard",
+    keyboardSchema
+);
+
+export default {
+    productModel,
+    allInOneModel,
+    cpuModel,
+    monitorModel,
+    mouseModel,
+    keyboardModel
+}
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,10 +1,14 @@
 import express from "express";
+import multer from "multer";
 import productController from "../controller/productController.js";
+const upload = multer({ dest: 'uploads/' });
 
 export const productRouter = express.Router();
 
 productRouter.post('/addProduct', productController.addProduct);
-productRouter.get('/getProducts', productController.getProducts);
+productRouter.get('/getProducts/:type', productController.getProducts);
 productRouter.put('/updateProduct/:productId', productController.updateProduct);
 productRouter.get('/getProduct/:productId', productController.getProduct);
 productRouter.delete('/deleteProduct/:productId', productController.deleteProduct);
+productRouter.post('/addProductByExcel', upload.single('file'), productController.addProductByExcel);
+productRouter.get('/getProductGroups', productController.getProductGroups);
